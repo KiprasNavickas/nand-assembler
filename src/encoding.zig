e8609b30f28cf2a37ac2e5b161e0ac84c72daed4
Kipras Navickas <kipras.navickas@gmail.com>
2026-04-20 03:13:51 +1000

    Make encodeA take a string

--- src/encoding.zig
+++ src/encoding.zig
@@ -3,12 +3,13 @@ const types = @import("types.zig");
 
 const AssemblerError = types.AssemblerError;
 
-pub fn encodeA(value: u16) !u16 {
-    if (value >= 1 << 15) {
+pub fn encodeA(value: []const u8) !u16 {
+    const val = try std.fmt.parseUnsigned(u16, value, 10);
+    if (val >= 1 << 15) {
         return AssemblerError.InvalidA;
     }
 
-    return value;
+    return val;
 }
 
 pub fn encodeC(dest: []const u8, comp: []const u8, jump: []const u8) !u16 {
