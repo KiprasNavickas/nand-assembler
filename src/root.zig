90bc6c5c2c84fc19fdbdf430394f32950d460bf7
Kipras Navickas <kipras.navickas@gmail.com>
2026-04-22 23:07:07 +0300

    Use refAllDecls to discover all tests.

--- src/root.zig
+++ src/root.zig
@@ -1,3 +1,5 @@
+const std = @import("std");
+
 const types = @import("types.zig");
 const parser = @import("parser.zig");
 pub const encoding = @import("encoding.zig");
@@ -7,3 +9,7 @@ pub const Parser = parser.Parser;
 pub const AssemblerError = types.AssemblerError;
 pub const CommandType = types.CommandType;
 pub const assemble = @import("assembler.zig").assemble;
+
+test {
+    std.testing.refAllDecls(@This());
+}
